11bbd624e995d8406770bb09dc7bd1264a982a3e
maya <amacrk@i.softbank.jp>
2020-05-12 00:52:17 +0900

    create drum sound

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    //ドラムサウンドのファイル
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // ドラムタップで呼び出し
+    @IBAction func tapDrumButton() {
+        
+        //ドラムの音を巻き戻す
+        drumSoundPlayer.currentTime = 0
+        
+        //ドラムの音を再生
+        drumSoundPlayer.play()
+    }
 
 }
 
