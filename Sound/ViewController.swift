21be0a1b69693f40c0b88dbb6012f4687d3cea8e
maya <amacrk@i.softbank.jp>
2020-05-12 01:33:21 +0900

    Complete sound

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -14,8 +14,20 @@ class ViewController: UIViewController {
     //ドラムを表示する変数
     @IBOutlet var drumButton: UIButton!
     
+    //ピアノを表示する変数
+    @IBOutlet var pianoButton: UIButton!
+    
+    //ギターを表示する変数
+    @IBOutlet var guitarButton: UIButton!
+    
     //ドラムサウンドのファイル
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    
+    //ピアノサウンドのファイル
+    let pianoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
+    
+    //ギターサウンドのファイル
+    let guitarSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "guitarSound")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +46,47 @@ class ViewController: UIViewController {
         drumSoundPlayer.play()
     }
     
+    @IBAction func touchDownPianoButton() {
+           
+           //ピアノが鳴る時の画像
+           pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+           //ピアノの音を巻き戻す
+           pianoSoundPlayer.currentTime = 0
+           
+           //ピアノの音を再生
+           pianoSoundPlayer.play()
+       }
+    
+    @IBAction func touchDownGuitarButton() {
+           
+           //ギターが鳴る時の画像
+           guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+           //ギターの音を巻き戻す
+           guitarSoundPlayer.currentTime = 0
+           
+           //ギターの音を再生
+           guitarSoundPlayer.play()
+       }
+    
     //タッチが終わる時の画像
     @IBAction func touchUpDrumButton() {
         
         //ドラムが鳴っていない画像
         drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
+    
+    //タッチが終わる時の画像
+    @IBAction func touchUpPianoButton() {
+        
+        //ドラムが鳴っていない画像
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+    
+    //タッチが終わる時の画像
+    @IBAction func touchUpGuitarButton() {
+        
+        //ドラムが鳴っていない画像
+        guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
     }
 
 }
