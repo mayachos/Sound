0fcd340b68b777d7415780799ed6046b733fe5dd
maya <amacrk@i.softbank.jp>
2020-05-12 01:08:57 +0900

    change drum image

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -11,6 +11,9 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    //ドラムを表示する変数
+    @IBOutlet var drumButton: UIButton!
+    
     //ドラムサウンドのファイル
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
 
@@ -20,13 +23,22 @@ class ViewController: UIViewController {
     }
 
     // ドラムタップで呼び出し
-    @IBAction func tapDrumButton() {
+    @IBAction func touchDownDrumButton() {
         
+        //ドラムが鳴る時の画像
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         //ドラムの音を巻き戻す
         drumSoundPlayer.currentTime = 0
         
         //ドラムの音を再生
         drumSoundPlayer.play()
+    }
+    
+    //タッチが終わる時の画像
+    @IBAction func touchUpDrumButton() {
+        
+        //ドラムが鳴っていない画像
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
     }
 
 }
